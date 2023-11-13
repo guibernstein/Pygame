70b7d8907c8f762db93c5766cc1941415a2562bc
guibernstein <guilhermehb1@al.insper.edu.br>
2023-11-13 10:50:55 -0300

    Caption e window

--- JOGO.py
+++ JOGO.py
@@ -2,11 +2,21 @@ import pygame
 
 pygame.init()
 
-window = pygame.display.set_mode((500,400))
+WIDTH = 1000
+HEIGHT = 800
+
+window = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption('DEADLY TARGET')
 
 game = True
+
+font = pygame.font.SysFont('Comic Sans', 48)
+text = font.render('Deadly Target', True, (0, 0, 255))
 
 while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
+
+    window.blit(text, (400, 400))
+    pygame.display.update()
