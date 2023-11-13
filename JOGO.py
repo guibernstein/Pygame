a36cbb48a9e063b7bc0291ac61337f6df543ab61
guibernstein <guilhermehb1@al.insper.edu.br>
2023-11-13 11:15:09 -0300

    zumbi

--- JOGO.py
+++ JOGO.py
@@ -11,7 +11,16 @@ pygame.display.set_caption('DEADLY TARGET')
 game = True
 
 font = pygame.font.SysFont('Comic Sans', 48)
+
 text = font.render('Deadly Target', True, (0, 0, 255))
+
+largura_zumbi = 10
+altura_zumbi = 4
+
+zumbi = pygame.image.load('zumbi.png').convert_alpha()
+zumbi = pygame.transform.scale(zumbi, (largura_zumbi,altura_zumbi))
+
+
 
 while game:
     for event in pygame.event.get():
