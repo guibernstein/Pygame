66cbacae9ec4cd35a458681dafe3c074d24d3ada
guibernstein <guilhermehb1@al.insper.edu.br>
2023-11-13 10:20:03 -0300

    while game

--- JOGO.py
+++ JOGO.py
@@ -4,3 +4,9 @@ pygame.init()
 
 window = pygame.display.set_mode((500,400))
 
+game = True
+
+while game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
