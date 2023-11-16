196efa52b581f36e020516da79f7c40a1ac67fb4
guibernstein <guilhermehb1@al.insper.edu.br>
2023-11-16 14:58:59 -0300

    background + incorporação do zumbi na tela

--- JOGO.py
+++ JOGO.py
@@ -8,24 +8,30 @@ HEIGHT = 800
 window = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption('DEADLY TARGET')
 
-game = True
-
+largura_zumbi = 50
+altura_zumbi = 50
 font = pygame.font.SysFont('Comic Sans', 48)
-
-text = font.render('Deadly Target', True, (0, 0, 255))
-
-largura_zumbi = 10
-altura_zumbi = 4
-
+background = pygame.image.load('fundo.png').convert()
+background = pygame.transform.scale(background, (WIDTH,HEIGHT))
 zumbi = pygame.image.load('zumbi.png').convert_alpha()
 zumbi = pygame.transform.scale(zumbi, (largura_zumbi,altura_zumbi))
 
-
+game = True
+zumbi_x = 500
+zumbi_y = 500
+zumbi_speedx = 0.2
+zumbi_speedy = 0.2
 
 while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
 
-    window.blit(text, (400, 400))
+    zumbi_x += zumbi_speedx
+    zumbi_y += zumbi_speedy
+
+    window.blit(background, (0, 0))
+    window.blit(zumbi, (zumbi_x,zumbi_y))
     pygame.display.update()
+
+pygame.quit()
