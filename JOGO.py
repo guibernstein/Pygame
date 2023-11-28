ecc83026ad44552b854ee40052c0fef329926cd3
guibernstein <guilhermehb1@al.insper.edu.br>
2023-11-28 18:03:28 -0300

    Criando o score e adiciona zumbis a cada 2000 pontos

--- JOGO.py
+++ JOGO.py
@@ -18,6 +18,7 @@ zumbi_img = pygame.image.load('zumbi.png').convert_alpha()
 zumbi_img = pygame.transform.scale(zumbi_img, (largura_zumbi,altura_zumbi))
 mira_img = pygame.image.load('Mira.png').convert_alpha()
 mira_img = pygame.transform.scale(mira_img, (80,80))
+score_font = pygame.font.Font('HelpMe.ttf',50)
 
 class Zumbi(pygame.sprite.Sprite):
 
@@ -40,7 +41,7 @@ class Zumbi(pygame.sprite.Sprite):
     
     def update(self):
 
-        tamanho_aumento = self.rect.y / 10
+        tamanho_aumento = self.rect.y / 5
         novo_tamanho = (int(self.tamanho_original[0] + tamanho_aumento), int(self.tamanho_original[1] + tamanho_aumento))
         self.image = pygame.transform.scale(zumbi_img, novo_tamanho)
         self.rect.x += self.speedx
@@ -72,6 +73,9 @@ for i in range(3):
 mira = Mira(mira_img)
 all_sprites.add(mira)
 
+score = 0
+zumbi_added = False
+
 while game:
     clock.tick(FPS)
     for event in pygame.event.get():
@@ -83,6 +87,20 @@ while game:
             for zumbi in all_zumbis:
                 if zumbi.rect.collidepoint(pygame.mouse.get_pos()):
                     zumbi.reset()
+                    score += 100
+    
+
+    if score != 0 and score % 2000 == 0 and not zumbi_added:
+        novo_zumbi = Zumbi(zumbi_img)
+        all_sprites.add(novo_zumbi)
+        all_zumbis.add(novo_zumbi)
+        zumbi_added = True
+
+        nova_mira = Mira(mira_img)
+        all_sprites.add(nova_mira)
+
+    if score % 2000 != 0:
+        zumbi_added = False
 
     all_sprites.update()
 
@@ -92,6 +110,11 @@ while game:
 
     window.blit(background, (0, 0))
     all_sprites.draw(window)
+
+    text_surface = score_font.render("SCORE:{:06d}".format(score), True, (255, 0, 0))
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (WIDTH / 2,  10)
+    window.blit(text_surface, text_rect)
 
     pygame.display.update()
 
